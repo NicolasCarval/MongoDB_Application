65d83976c154c4d7cb9c798a02bec13c52342bdc
Nicolas Carval <nicolascarval@yahoo.fr>
2022-12-10 17:54:09 +0100

    (FEAT : user, admin, analyst) : graphs available and admin partially complete

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ Created on Fri Dec 24 10:15:54 2021
 
 @author: Nicolas Carval
 """
+#%% Libraries and MongoDB connection
 
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 from ssh_pymongo import MongoSession
 import pandas as pd
+import json
 
+import plotly
+import plotly.express as px
+
+# Connecting to MongoDB
 session = MongoSession(
     "MESIIN592022-0055.westeurope.cloudapp.azure.com",
     port=22,
@@ -19,36 +25,50 @@ session = MongoSession(
     uri="mongodb://MESIIN592022-0055:30000/")
 
 movies = session.connection['movies']
-#movies_year=session.connection["movies"]["Movies_sharding_year"]
 #session.stop()
 
+#%% STATIC PAGES
+
+# method to send query to collection
 def run_query(movies, pipeline):
     return movies.Movies_sharding_name.aggregate(pipeline,allowDiskUse=True)
 
+#APPLICATION
 app = Flask(__name__)
 
+#Home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
+#User page
 @app.route('/User',methods=['GET'])
 def User():
-    # show the form, it wasn't submitted
     return render_template('User.html')
 
+#Analyst page
 @app.route('/Analyst',methods=['GET'])
 def Analyst():
     # show the form, it wasn't submitted
     return render_template('Analyst.html')
 
+# Results for User queries
 @app.route('/Answer',methods=['GET'])
 def Answer():
-    # show the form, it wasn't submitted
     return render_template('Answer.html')
 
+# Results for Analyst queries
+@app.route('/Answer2',methods=['GET'])
+def Answer2():
+    return render_template('Answer2.html')
+
+@app.route('/Admin',methods=['GET'])
+def Admin():
+    return render_template('Admin.html')
 
 
+#%% QUERIES
+    
 
 @app.route('/query1',methods=['POST'])
 def query1():
@@ -157,8 +177,20 @@ def query4():
     result = run_query(movies, pipeline)
     print(result._CommandCursor__data)
     output = pd.DataFrame(result._CommandCursor__data)
-    output.drop(columns=["_id"], inplace = True)
-    return render_template('Answer.html',tables=[output.to_html(classes='data')], titles=output.columns.values)
+    if output.shape[0] >=1:
+        print("-------------")
+        print(output.head())
+        print("-------------")
+        output.drop(columns=["_id"], inplace = True)        
+        output = output.iloc[0]
+        print(output[0][0])
+        temp = {"genre":[x["genre"] for x in output[0][0]], "prob":[x["prob"] for x in output[0][0]]}
+        output = pd.DataFrame(temp)
+        output.sort_values(ascending=False, by = ["prob"],inplace=True)
+    fig = px.bar(output, x='genre', y='prob', barmode='group', color_discrete_sequence = ["#A00C40"]*len(output))  
+    fig.update_layout({'plot_bgcolor': 'rgba(62, 78, 80, 10)'})
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('Answer.html', graphJSON=graphJSON)
 
 @app.route('/query5',methods=['POST'])
 def query5():
@@ -249,7 +281,13 @@ def query7():
     output.drop(columns=["_id",'top'], inplace = True)
     output.sort_values(by=["year"], inplace = True)
     output.reset_index(drop=True)
-    return render_template('Answer2.html', tables=[output.to_html(classes='data')], titles=output.columns.values)
+    output = output[["year","genre","note_moyenne"]]
+    print(output)
+    fig = px.bar(output, x='note_moyenne', y="year", barmode='group', color="genre", orientation="h")  
+    fig.update_layout({'plot_bgcolor': 'rgba(62, 78, 80, 10)','xaxis_range':[0,10], 'yaxis':{"tickmode" : 'array',
+        "tickvals" : output.year.values}})
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template('Answer2.html', graphJSON=graphJSON)
 
 @app.route('/query8',methods=['POST'])
 def query8():
@@ -298,5 +336,16 @@ def query8():
 
     return render_template('Answer2.html',tables=[output.to_html(classes='data')], titles=output.columns.values)
 
+
+@app.route('/Admin',methods=['POST'])
+def Admin_query():
+    result = movies.Movies_sharding_name.index_information()
+    aha = [result[x]["key"][0][0] for x,v in result.items()]
+    rep = " - ".join(aha)
+    print(rep)
+
+    return render_template('Admin.html',keys = rep)
+
+#%% MAIN
 if __name__ == "__main__":
     app.run(host='localhost', port=8989)
